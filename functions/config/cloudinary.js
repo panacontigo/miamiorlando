888c42ec627deb5e13426040c302e9e5214cc771
panacontigo <68999307+panacontigo@users.noreply.github.com>
2025-01-30 12:52:57 -0400

    Delete functions directory

--- functions/config/cloudinary.js
+++
@@ -1,9 +0,0 @@
-const cloudinary = require('cloudinary').v2;
-
-cloudinary.config({
-    cloud_name: 'ddcmguxaa',
-    api_key: '265314563392489',
-    api_secret: '-j63BsklickXAWzcwy029D6h_kU'
-});
-
-module.exports = cloudinary; 
